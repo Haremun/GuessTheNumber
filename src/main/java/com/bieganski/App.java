93b766136eef67795deebe64bd5e59d566d55487
Kamil <kamilbieg96@gmail.com>
2019-12-11 21:43:09 +0100

    Working impera Created game logic with just one class to illustrate main conecept

--- src/main/java/com/bieganski/App.java
+++ src/main/java/com/bieganski/App.java
@@ -1,13 +1,30 @@
 package com.bieganski;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
- * Hello world!
+ * Main concept of game "Guess the number"
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Guess the Number!");
+        int randomNumber = new Random().nextInt(100) + 1;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Your number: ");
+            int userNumber = scanner.nextInt();
+            if (userNumber < randomNumber)
+                System.out.println("Your number is too low");
+            else if (userNumber > randomNumber)
+                System.out.println("Your number is too high");
+            else {
+                System.out.println("You are correct!");
+                break;
+            }
+        } while (true);
     }
 }
